eef2c90ce376817104af3e8b76a28f264f2a4e17
Andre <dreoporto57@gmail.com>
2021-07-07 06:40:46 -0400

    dre-moe basic time-delay version

--- dre-moe.py
+++ dre-moe.py
@@ -1,32 +1,48 @@
 # noinspection PyUnresolvedReferences
 from picamera import PiCamera
+
+import uuid
 from time import sleep
 import os.path
 
 MAIN_DIRECTORY = 'media-files'
-TEMP_IMAGE_FILE = 'dre-moe-temp.jpg'
-WAIT_TIME = 5
+TEMP_IMAGE_FILE = 'dre-moe-temp'
+WAIT_TIME = 30
+LOOP = True
 
 
-def capture_image(path: str, pause_length: int = 0) -> None:
+def capture_image(path: str) -> None:
     camera = PiCamera()
     camera.led = False
 
     try:
-        print(f'taking photo in {pause_length} seconds...')
-        sleep(pause_length)
-
-        print('CLICK!')
         camera.capture(path)
     finally:
         camera.close()
 
 
-def main() -> None:
+def do_motion_capture():
     if not os.path.exists(MAIN_DIRECTORY):
         os.mkdir(MAIN_DIRECTORY)
 
-    capture_image(os.path.join(MAIN_DIRECTORY, TEMP_IMAGE_FILE), WAIT_TIME)
+    filename = f'{TEMP_IMAGE_FILE}-{uuid.uuid4()}.jpg'
+    filepath = os.path.join(MAIN_DIRECTORY, filename)
+
+    print('CLICK!')
+
+    capture_image(filepath)
+
+
+def main() -> None:
+
+    while True:
+        do_motion_capture()
+
+        if not LOOP:
+            break
+
+        print(f'taking photo in {WAIT_TIME} seconds...')
+        sleep(WAIT_TIME)
 
 
 if __name__ == '__main__':
