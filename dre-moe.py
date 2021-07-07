346dc23958f96a48cf9c2c02074a7197f611f27c
Andre <dreoporto57@gmail.com>
2021-07-07 18:17:41 -0400

    add timestamp to filename and cmd output

--- dre-moe.py
+++ dre-moe.py
@@ -1,18 +1,21 @@
+import datetime
+import os.path
+import sys
+import uuid
+from time import sleep
+
 # noinspection PyUnresolvedReferences
 from picamera import PiCamera
 
-import uuid
-from time import sleep
-import os.path
-
 MAIN_DIRECTORY = 'media-files'
 TEMP_IMAGE_FILE = 'dre-moe-temp'
-WAIT_TIME = 30
+WAIT_TIME = 20
 LOOP = True
 
 
 def capture_image(path: str) -> None:
     camera = PiCamera()
+    camera.resolution = (1280, 720)
     camera.led = False
 
     try:
@@ -22,15 +25,23 @@ def capture_image(path: str) -> None:
 
 
 def do_motion_capture():
+
+    display_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    time_suffix = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+
     if not os.path.exists(MAIN_DIRECTORY):
         os.mkdir(MAIN_DIRECTORY)
 
-    filename = f'{TEMP_IMAGE_FILE}-{uuid.uuid4()}.jpg'
+    # i like guids
+    filename = f'{TEMP_IMAGE_FILE}-{time_suffix}-{uuid.uuid4()}.jpg'
     filepath = os.path.join(MAIN_DIRECTORY, filename)
 
-    print('CLICK!')
+    print(f'{display_time}\tCLICK!\tnext photo in {WAIT_TIME} seconds...', end='\r')
+    sys.stdout.flush()
 
     capture_image(filepath)
+
+    sleep(WAIT_TIME)
 
 
 def main() -> None:
@@ -40,9 +51,6 @@ def main() -> None:
 
         if not LOOP:
             break
-
-        print(f'taking photo in {WAIT_TIME} seconds...')
-        sleep(WAIT_TIME)
 
 
 if __name__ == '__main__':
