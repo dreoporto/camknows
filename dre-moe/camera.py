5f7dc61c4c3ea06aabdfe060ca5e00928b804291
Andre Oporto <andreoporto@gmail.com>
2021-07-14 07:52:34 -0400

    implement json config for settings

--- dre-moe/camera.py
+++ dre-moe/camera.py
@@ -10,40 +10,31 @@ from time import sleep
 import picamera
 
 CONFIG_FILE = 'dre-moe-config.json'
-# TODO AEO NEXT: move config settings to JSON
-# CAMERA / APP CONFIG SETTINGS
-MAIN_DIRECTORY = 'media-files'
-IMAGE_FILE = 'dre-moe'
-WAIT_TIME = 2  # TODO AEO TEMP
-LOOP = False  # TODO AEO TEMP
-AWB_DELAY = 3  # allow awb to catch up
-RESOLUTION = (2592, 1944)
-SHOW_TIMESTAMP = True
-# low light config settings
-ISO = 640
-# default is 30 (30 fps); USE RANGE INSTEAD
-# camera.framerate = Fraction(1, 8)
-FRAMERATE_RANGE = (Fraction(1, 6), Fraction(30, 1))
-SHUTTER_SPEED = 125000
 
 # TODO AEO NEXT: change most PRINTs to LOG to allow disable/level
 
 
 class Camera:
 
-    # def __init__(self):
-    #     with open(CONFIG_FILE) as json_file:
-    #         self.config = json.load(json_file)
+    def __init__(self):
+        with open(CONFIG_FILE) as json_file:
+            self.config = json.load(json_file)
 
     def _setup_camera(self, camera) -> None:
 
         print(f'{self.get_timestamp()}\tSetup PiCamera')
-        camera.resolution = RESOLUTION
+        resolution_width = self.config['resolution_width']
+        resolution_height = self.config['resolution_height']
+        camera.resolution = (resolution_width, resolution_height)
         camera.led = False
 
-        camera.iso = ISO
-        camera.framerate_range = FRAMERATE_RANGE
-        camera.shutter_speed = SHUTTER_SPEED
+        # low light settings
+        camera.iso = self.config['iso']
+        # (Fraction(1, 6), Fraction(30, 1))
+        framerate_range_from = Fraction(self.config['framerate_range_from'])
+        framerate_range_to = Fraction(self.config['framerate_range_to'])
+        camera.framerate_range = (framerate_range_from, framerate_range_to)
+        camera.shutter_speed = self.config['shutter_speed']
 
         # print camera settings
         print(f'{self.get_timestamp()}\tCAMERA SETTINGS:')
@@ -58,16 +49,19 @@ class Camera:
 
     def _capture_image(self, camera, directory_path: str) -> None:
 
-        print(f'{self.get_timestamp()}\tAWB Delay for {AWB_DELAY} seconds')
-        sleep(AWB_DELAY)
+        # allow awb to catch up
+        awb_delay = self.config['awb_delay']
+        print(f'{self.get_timestamp()}\tAWB Delay for {awb_delay} seconds')
+        sleep(awb_delay)
 
         print(f'{self.get_timestamp()}\tTaking Photo...')
-        if SHOW_TIMESTAMP:
+        if self.config['show_timestamp']:
             camera.annotate_background = picamera.Color('black')
             camera.annotate_text = self.get_timestamp()
 
+        image_file = self.config['image_file']
         time_suffix = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-        filename = f'{IMAGE_FILE}-{time_suffix}-{str(uuid.uuid4())[:8]}.jpg'
+        filename = f'{image_file}-{time_suffix}-{str(uuid.uuid4())[:8]}.jpg'
         filepath = os.path.join(directory_path, filename)
 
         camera.capture(filepath)
@@ -77,8 +71,9 @@ class Camera:
     def _do_motion_capture(self):
 
         # setup directory and output format
+        main_directory = self.config['main_directory']
         subdirectory = datetime.datetime.now().strftime('%Y/%m/%d')
-        directory_path = os.path.join(MAIN_DIRECTORY, subdirectory)
+        directory_path = os.path.join(main_directory, subdirectory)
 
         if not os.path.exists(directory_path):
             os.makedirs(directory_path)
@@ -93,15 +88,18 @@ class Camera:
                 camera.close()
                 print(f'{self.get_timestamp()}\tCamera Closed')
 
-        print(f'{self.get_timestamp()}\tsleeping for {WAIT_TIME} seconds')
-        sleep(WAIT_TIME)
+        wait_time = self.config['wait_time']
+        print(f'{self.get_timestamp()}\tsleeping for {wait_time} seconds')
+        sleep(wait_time)
 
     def start_motion_capture_loop(self):
+
+        do_loop = self.config['do_loop']
 
         while True:
             self._do_motion_capture()
 
-            if not LOOP:
+            if not do_loop:
                 break
 
     # noinspection PyMethodMayBeStatic
