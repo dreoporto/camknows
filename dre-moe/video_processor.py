fe3d584f2d91b7a86914120504eb16d0ad6b06b2
Andre Oporto <andreoporto@gmail.com>
2021-09-01 08:14:06 -0400

    improve images_directory parsing in video_processor

--- dre-moe/video_processor.py
+++ dre-moe/video_processor.py
@@ -12,7 +12,8 @@ class VideoProcessor:
     def __init__(self):
         self.script_directory = os.path.dirname(os.path.abspath(__file__))
 
-    def convert_to_video(self, images_directory: str, output_file: str, extension: str):
+    # noinspection PyMethodMayBeStatic
+    def convert_to_video(self, images_directory: str, output_file: str, extension: str) -> None:
 
         images_list: List[str] = []
         for image in os.listdir(images_directory):
@@ -26,7 +27,7 @@ class VideoProcessor:
         character_code = cv2.VideoWriter_fourcc(*'MP4V')
         frames_per_second = 1
 
-        frame_image = cv2.imread(os.path.join(self.script_directory, images_directory, images_list[0]))
+        frame_image = cv2.imread(os.path.join(images_directory, images_list[0]))
         frame_height, frame_width, _ = frame_image.shape
 
         video = cv2.VideoWriter(output_file + '.mp4', character_code, frames_per_second, (frame_width, frame_height))
@@ -39,7 +40,7 @@ class VideoProcessor:
                 timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
                 if image_index % 10 == 0:
                     print(f'{timestamp}\t\tprocessing image {image_index}')
-                video.write(cv2.imread(os.path.join(self.script_directory, images_directory, image)))
+                video.write(cv2.imread(os.path.join(images_directory, image)))
         finally:
             cv2.destroyAllWindows()
             video.release()
@@ -60,14 +61,10 @@ def parse_args(args: List[str]) -> Tuple[str, str, str]:
     images_directory = args[0]
     output_file = args[1]
     extension = args[2]
+    full_path = parse_directory_path(images_directory)
 
-    if images_directory[0] == '/':
-        images_directory = images_directory[1:]
-
-    full_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), images_directory)
-
-    if not os.path.exists(full_path):
-        print('Invalid directory argument:', full_path)
+    if full_path == '':
+        print('Invalid directory argument:', images_directory)
         print(format_message)
         return '', '', ''
 
@@ -79,7 +76,24 @@ def parse_args(args: List[str]) -> Tuple[str, str, str]:
     return full_path, output_file, extension
 
 
-def main():
+def parse_directory_path(directory_path: str) -> str:
+    parsed_path = ''
+
+    if os.path.exists(directory_path):
+        parsed_path = directory_path
+    else:
+        if directory_path[0] == '/':
+            parsed_path = directory_path[1:]
+
+        parsed_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), parsed_path)
+
+        if not os.path.exists(parsed_path):
+            parsed_path = ''
+
+    return parsed_path
+
+
+def main() -> None:
     images_directory, output_file, extension = parse_args(sys.argv[1:])
     if images_directory == '':
         return
